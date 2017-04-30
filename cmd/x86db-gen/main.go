ffca16ac3a47a49e91b89c72a3f863f5c955bf13
Damien Lespiau <damien.lespiau@intel.com>
2017-05-01 00:50:07 +0100

    gen: CMPPS and CMPSS have 3 operands in the go assembly
    Signed-off-by: Damien Lespiau <damien.lespiau@intel.com>

--- cmd/x86db-gen/main.go
+++ cmd/x86db-gen/main.go
@@ -9,7 +9,26 @@ import (
 	"github.com/dlespiau/x86db"
 )
 
+func nasmOpcodeToPlan9(op string) string {
+	/*
+	 * SSE
+	 */
+	switch op {
+	// The condition is encoded as an imm8 operand of CMPPS
+	case "CMPEQPS", "CMPLTPS", "CMPLEPS", "CMPUNORDPS", "CMPNEQPS", "CMPNLTPS",
+		"CMPNLEPS", "CMPORDPS":
+		return "CMPPS"
+	// The condition is encoded as an imm8 operand of CMPSS.
+	case "CMPEQSS", "CMPLTSS", "CMPLESS", "CMPUNORDSS", "CMPNEQSS", "CMPNLTSS",
+		"CMPNLESS", "CMPORDSS":
+		return "CMPSS"
+	}
+
+	return op
+}
+
 func isAlreadyKnown(name string) bool {
+	name = nasmOpcodeToPlan9(name)
 	for _, opcode := range Anames {
 		if name == opcode {
 			return true
